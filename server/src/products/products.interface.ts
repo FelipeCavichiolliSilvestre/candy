1b753580a956af6592b9f99351b707e6a1c84985
Felipe Cavichiolli Silvestre <felipecavichiollisilvestre@gmail.com>
2023-05-18 10:03:12 -0300

    add products service interface

--- server/src/products/products.interface.ts
+++ server/src/products/products.interface.ts
@@ -1,4 +1,39 @@
 import { Injectable } from "@nestjs/common";
+import { AdjustmentType, Product } from "@prisma/client";
 
 @Injectable()
-export abstract class IProductsService {}
+export abstract class IProductsService {
+  abstract register(data: RegisterProductInput): Promise<Product>;
+  abstract updateInfo(data: UpdateProductInfoInput): Promise<Product>;
+  abstract updateStock(data: UpdateProductStockInput): Promise<Product>;
+  abstract updatePrice(data: UpdateProductPriceInput): Promise<Product>;
+  abstract list(page?: number): Promise<ListProductsOutput>;
+}
+
+export type RegisterProductInput = {
+  name: string;
+  description: string;
+  price: number;
+};
+
+export type UpdateProductInfoInput = {
+  productId: number;
+  name?: string;
+  description?: string;
+};
+
+export type UpdateProductStockInput = {
+  productId: number;
+  value: number;
+  type: AdjustmentType;
+};
+
+export type UpdateProductPriceInput = {
+  productId: number;
+  value: number;
+};
+
+export type ListProductsOutput = {
+  products: Product[];
+  hasNextPage: number;
+};
