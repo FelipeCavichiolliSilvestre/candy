11f41831aead16398fdec36b86198a19a2c3b14a
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2023-05-25 13:03:10 -0300

    add base products routes

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post, Put } from "@nestjs/common";
 import { IProductsService } from "./products.interface";
 
 @Controller("/products")
 export class ProductsController {
   constructor(private productsService: IProductsService) {}
+
+  @Get("/")
+  async getProduct() {}
+
+  @Post("/")
+  async createProduct() {}
+
+  @Put("/:productId/info")
+  async updateProductInfo() {}
+
+  @Put("/:productId/price")
+  async updateProductPrice() {}
+
+  @Put("/:productId/quantity")
+  async updateProductQuantity() {}
 }
