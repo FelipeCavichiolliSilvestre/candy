5f2c3bcff67aa8fa28598766135702ac57cc8c0e
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2023-05-25 22:01:38 -0300

    add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AuthGuard } from "./auth";
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalGuards(app.select(AppModule).get(AuthGuard));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  const swagger = new DocumentBuilder()
+    .setTitle("Candy API")
+    .setDescription("The ecommerce candy API description")
+    .setVersion("0.0.1")
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup("docs", app, document);
 
   await app.listen(process.env.APP_PORT ?? 8080);
 }
