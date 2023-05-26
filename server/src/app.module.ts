fcc5bc0338961f474747e18a0cfa3502a6a21463
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2023-05-26 12:48:49 -0300

    serve api definition

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { ServeStaticModule } from "@nestjs/serve-static";
 import { AuthModule } from "./auth";
 import { EmployeesModule } from "./employees";
 import { OrdersModule } from "./orders";
 import { PrismaModule } from "./prisma";
 import { ProductsModule } from "./products";
+import { join } from "path";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "static"),
+      serveRoot: "/docs",
+    }),
     PrismaModule,
     AuthModule,
     EmployeesModule,
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import { AuthGuard } from "./auth";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,8 @@ async function bootstrap() {
     .setVersion("0.0.1")
     .build();
   const document = SwaggerModule.createDocument(app, swagger);
+  if (!existsSync("./static")) mkdirSync("./static");
+  writeFileSync("./static/api.json", JSON.stringify(document));
   SwaggerModule.setup("docs", app, document);
 
   await app.listen(process.env.APP_PORT ?? 8080);
