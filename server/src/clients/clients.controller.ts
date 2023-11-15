42f9297d336c7f5f63f4f54436a1c32999381a16
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2023-11-15 12:04:24 -0300

    add responses types in ClientsController

--- server/src/clients/clients.controller.ts
+++ server/src/clients/clients.controller.ts
@@ -1,17 +1,25 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
-import { AllowUnauthenticated } from "src/auth";
+import { AllowUnauthenticated, IAuthService } from "src/auth";
 import { IClientsService } from "./clients.interface";
 import { CreateClientBodyDTO } from "./dtos";
+import { ClientLoginResponseDTO } from "../auth/dtos";
 
 @ApiTags("clients")
 @Controller("/clients")
 export class ClientsController {
-  constructor(private clientsService: IClientsService) {}
+  constructor(
+    private clientsService: IClientsService,
+    private authService: IAuthService
+  ) {}
 
   @Post("/")
   @AllowUnauthenticated()
-  async create(@Body() body: CreateClientBodyDTO) {
-    return await this.clientsService.signUp(body);
+  async create(
+    @Body() body: CreateClientBodyDTO
+  ): Promise<ClientLoginResponseDTO> {
+    await this.clientsService.signUp(body);
+
+    return this.authService.login(body.username, body.password, "client");
   }
 }
